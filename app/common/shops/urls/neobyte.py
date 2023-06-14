c3d35685010e967b929ac0b75d88f0c71e787569
MDaniel592 <mmdaniel@protonmail.com>
2023-06-14 22:15:18 +0200

    Scrape all pages for each category

--- app/common/shops/urls/neobyte.py
+++ app/common/shops/urls/neobyte.py
@@ -1,10 +1,10 @@
 urls = {
     "GPU": "https://www.neobyte.es/tarjetas-graficas-111?order=product.position.asc&resultsPerPage=9999999&from-xhr",
-    "CPU": "https://www.neobyte.es/tarjetas-graficas-107?order=product.position.asc&resultsPerPage=9999999&from-xhr",
-    "Motherboard": "https://www.neobyte.es/tarjetas-graficas-106?order=product.position.asc&resultsPerPage=9999999&from-xhr",
-    "PSU": "https://www.neobyte.es/tarjetas-graficas-113?order=product.position.asc&resultsPerPage=9999999&from-xhr",
-    "RAM": "https://www.neobyte.es/tarjetas-graficas-108?order=product.position.asc&resultsPerPage=9999999&from-xhr",
-    "Storage": "https://www.neobyte.es/tarjetas-graficas-110?order=product.position.asc&resultsPerPage=9999999&from-xhr",
-    "Chassis": "https://www.neobyte.es/tarjetas-graficas-112?order=product.position.asc&resultsPerPage=9999999&from-xhr",
-    "CPU Cooler": "https://www.neobyte.es/tarjetas-graficas-109?order=product.position.asc&resultsPerPage=9999999&from-xhr",
+    "CPU": "https://www.neobyte.es/procesadores-107?order=product.position.asc&resultsPerPage=9999999&from-xhr",
+    "Motherboard": "https://www.neobyte.es/placas-base-106?order=product.position.asc&resultsPerPage=9999999&from-xhr",
+    "PSU": "https://www.neobyte.es/fuentes-de-alimentacion-113?order=product.position.asc&resultsPerPage=9999999&from-xhr",
+    "RAM": "https://www.neobyte.es/memorias-ram-108?order=product.position.asc&resultsPerPage=9999999&from-xhr",
+    "Storage": "https://www.neobyte.es/discos-duros-110?order=product.position.asc&resultsPerPage=9999999&from-xhr",
+    "Chassis": "https://www.neobyte.es/cajas-de-ordenador-112?order=product.position.asc&resultsPerPage=9999999&from-xhr",
+    "CPU Cooler": "https://www.neobyte.es/ventilacion-109?order=product.position.asc&resultsPerPage=9999999&from-xhr",
 }
